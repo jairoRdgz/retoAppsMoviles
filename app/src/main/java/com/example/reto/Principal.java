916753a3e5f28e1bd6dfd91cc0536a24bcbf6f07
Jairo <jairitox2000@gmail.com>
2021-04-26 17:17:45 -0500

    vojabes

---
+++ app/src/main/java/com/example/reto/Principal.java
@@ -0,0 +1,11 @@
+package com.example.reto;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Principal extends AppCompatActivity {
+    protected BuscarFragment buscas;
+    protected MapsFragment map;
+    protected MenuFragment menu;
+    protected Principal principal;
+
+}
